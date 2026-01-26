04acd2235bd805479be432cf73e6a6619613493e
Test User <danya_zav@test.com>
2026-01-26 22:08:55 +0300

    init decompiler

---
+++ attack_surface_search/decompiled_folder/sources/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/* JADX WARN: Classes with same name are omitted:
+  Main.class
+ */
+/* loaded from: project_library.jar:org/example/Main.class */
+public class Main {
+    public static void main(String[] strArr) {
+        System.out.printf("Hello and welcome!", new Object[0]);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
